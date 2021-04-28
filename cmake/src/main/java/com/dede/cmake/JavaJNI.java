bf737952b70552b1b4b17dae05b9f5b9b9d66c16
胡晟昊 <863116814@qq.com>
2021-04-28 18:53:58 +0800

    更新README

---
+++ cmake/src/main/java/com/dede/cmake/JavaJNI.java
@@ -0,0 +1,10 @@
+package com.dede.cmake;
+
+/**
+ * @author hsh
+ * @since 4/28/21 3:31 PM
+ */
+public class CMakeJavaLib {
+
+    public static native String callJNI();
+}
---
+++ cmake/src/main/java/com/dede/cmake/KotlinJNI.kt
@@ -0,0 +1,8 @@
+package com.dede.cmake
+
+/**
+ * @author hsh
+ * @since 4/28/21 3:54 PM
+ */
+class KotlinJNI {
+}
